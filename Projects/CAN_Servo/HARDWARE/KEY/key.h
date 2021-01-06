1a531a0196468f09af85b195762ffc731af5c3ba
yyg2011 <yangyige2011@126.com>
2021-01-06 14:06:01 +0800

    舵机实验

---
+++ Projects/CAN_Servo/HARDWARE/KEY/key.h
@@ -0,0 +1,5 @@
+#ifndef __KEY_H
+#define __KEY_H	 
+#include "sys.h"	
+void KEY_Init(void);
+#endif
---
+++ Projects/CAN_Servo/HARDWARE/LED/led.h
@@ -0,0 +1,5 @@
+#ifndef __LED_H
+#define __LED_H
+#include "sys.h"	
+void LED_Init(void);	 				    
+#endif
