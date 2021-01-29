7428e4beac3af4b1640c74870574549fb3581e2d
yyg2011 <yangyige2011@126.com>
2021-01-29 15:00:24 +0800

    基于Hal库的GY-53 IIC通信实验

--- Projects/Engineeing_Car/主板/Core/Inc/main.h
+++ Projects/Engineeing_Car/主板/Core/Inc/main.h
@@ -58,12 +58,20 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define VL53L0X_XSHUT_Pin GPIO_PIN_13
+#define VL53L0X_XSHUT_GPIO_Port GPIOC
+#define VL53L0X_SCL_Pin GPIO_PIN_14
+#define VL53L0X_SCL_GPIO_Port GPIOC
+#define VL53L0X_SDA_Pin GPIO_PIN_15
+#define VL53L0X_SDA_GPIO_Port GPIOC
 #define LED_Pin GPIO_PIN_0
 #define LED_GPIO_Port GPIOC
 #define LED0_Pin GPIO_PIN_1
 #define LED0_GPIO_Port GPIOC
 #define KEY_Pin GPIO_PIN_0
 #define KEY_GPIO_Port GPIOB
+#define PHOTOELECTRIC_SWITCH_Pin GPIO_PIN_15
+#define PHOTOELECTRIC_SWITCH_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Projects/Engineeing_Car/主板/Core/Src/gpio.c
+++ Projects/Engineeing_Car/主板/Core/Src/gpio.c
@@ -44,13 +44,23 @@ void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, VL53L0X_XSHUT_Pin|VL53L0X_SCL_Pin|VL53L0X_SDA_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, LED_Pin|LED0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PCPin PCPin PCPin */
+  GPIO_InitStruct.Pin = VL53L0X_XSHUT_Pin|VL53L0X_SCL_Pin|VL53L0X_SDA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PCPin PCPin */
   GPIO_InitStruct.Pin = LED_Pin|LED0_Pin;
@@ -64,6 +74,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(KEY_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = PHOTOELECTRIC_SWITCH_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(PHOTOELECTRIC_SWITCH_GPIO_Port, &GPIO_InitStruct);
 
 }
 
